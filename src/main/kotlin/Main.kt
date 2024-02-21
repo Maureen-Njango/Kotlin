1c7f9da93e170e9c596c8deee3a0b4fe4bb130c4
Maureen-Njango <maureennjangombira@gmail.com>
2024-02-21 08:56:18 +0300

    kotlin assignment

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,19 @@
+fun main(){
+    var name = "MaureenNjango"
+    println(name)
+    var age: Byte = 20
+    println(age)
+    var phoneNumber = "0715061242"
+    println(phoneNumber)
+    var weightInKg: Byte = 80
+    println(weightInKg)
+    var kenyan : Boolean = true
+    println(kenyan)
+    var nonkenyan : Boolean = false
+    println(nonkenyan)
+
+
+
+}
+
+
